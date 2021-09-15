643266c89e5ab9115c364928a7ef9a25c660a922
jaqi <jaq.cat@protonmail.ch>
2021-09-15 17:27:18 -0300

    flask-wtf

---
+++ application/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+
+class SignupForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
--- application/views/auth.py
+++ application/views/auth.py
@@ -3,30 +3,41 @@ from flask import render_template, request, redirect, url_for, Blueprint
 from application import app, db
 from application.models import User
 from application.util import hashpw
+from application.forms import LoginForm, SignupForm
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.get('/')
 def auth():
-    return render_template('auth.html', title='Auth')
+    login_form = LoginForm()
+    signup_form = SignupForm()
+    return render_template('auth.html', title='Auth',
+        login_form=login_form, signup_form=signup_form)
 
 @bp.post('/login')
 def signin():
-    res = User.query.filter_by(
-        email=request.form['email'],
-        password=hashpw(request.form['password'])
-    ).all()
-    if len(res) != 0:
-        res[0].signin(request.form['rember'])
-    return redirect(url_for('index.index'))
+    login_form = LoginForm()
+    if login_form.validate_on_submit:
+        res = User.query.filter_by(
+            email=login_form.email,
+            password=hashpw(login_form.password)
+        ).all()
+        if len(res) != 0:
+            res[0].signin(login_form.remember_me)
+        return redirect(url_for('index.index'))
+    return redirect(url_for('auth.auth'))
 
 @bp.post('/signup')
 def signup():
-    db.session.add(User(
-        request.form['name'],
-        request.form['email'],
-        request.form['password']))
-    db.session.commit()
-    return redirect(url_for('index.index'))
+    signup_form = SignupForm()
+    if signup_form.validate_on_submit():
+        db.session.add(User(
+            signup_form.name,
+            signup_form.email,
+            signup_form.password
+        ))
+        db.session.commit()
+        return redirect(url_for('index.index'))
+    return redirect(url_for('auth.auth'))
 
 app.register_blueprint(bp)
